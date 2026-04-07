a5f6eb14df2efcc3a173c461fc1c4e00d4e9858e
Michael L. Flegel <mlflegel@gmail.com>
2026-04-07 21:12:05 +0200

    Initial Commit

---
+++ Hab' ich wirklich so viel Zeit gespart?/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hab' ich wirklich so viel Zeit gespart?
+//
+//  Created by Michael L. Flegel on 07.04.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hab' ich wirklich so viel Zeit gespart?/Hab__ich_wirklich_so_viel_Zeit_gespart_App.swift
@@ -0,0 +1,17 @@
+//
+//  Hab__ich_wirklich_so_viel_Zeit_gespart_App.swift
+//  Hab' ich wirklich so viel Zeit gespart?
+//
+//  Created by Michael L. Flegel on 07.04.26.
+//
+
+import SwiftUI
+
+@main
+struct Hab__ich_wirklich_so_viel_Zeit_gespart_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
